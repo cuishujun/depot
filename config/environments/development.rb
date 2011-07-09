c52a2716ddc8c4c9b63eaa34c5be3814d5cafce5
Vincent <shujuncui@gmail.com>
2011-07-09 17:15:16 +0800

    Task h over

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,18 @@ Blog::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 456,
+    :domain => "domain.of.sender.net",
+    :authentication => "plain",
+    :user_name => "shujun.cui@cubead.com",
+    :password => "123456",
+    :enable_starttls_auto => true
+  }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
